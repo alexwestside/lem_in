6bf3d16343c62db3686c0c2515238820d329c6dc
orizhiy <sklif.85@gmail.com>
2017-04-18 21:51:06 +0300

    read

--- lem_in.h
+++ lem_in.h
@@ -3,5 +3,28 @@
 #define LEM_IN_LEM_IN_H_H
 
 #include "libft/libft.h"
+#include "libft/get_next_line.h"
+
+
+typedef struct	s_stdin
+{
+	char **std_in;
+
+}				t_stdin;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif //LEM_IN_LEM_IN_H_H
--- main.c
+++ main.c
@@ -1,8 +1,67 @@
 
 #include "lem_in.h"
 
+
+unsigned long int ft_twodem_strlen(char **str)
+{
+	char **p;
+
+	p = str;
+	while (*p)
+		p++;
+	return ((p - str) + 1);
+}
+
+
+char **ft_realloc(char **std_in, unsigned long int len)
+{
+	char **str;
+	int i = -1;
+
+	str = (char **)malloc(sizeof(char) * len + 1);
+	str[len] = NULL;
+	while (std_in[++i])
+		str[i] = std_in[i];
+	return (str);
+}
+
+void ft_read_write(t_stdin **input)
+{
+//	int i = 1;
+	char **str;
+	int fd = open("/nfs/2016/o/orizhiy/ClionProjects/lem_in/test", O_RDONLY);
+
+	(*input) = (t_stdin *)malloc(sizeof(t_stdin));
+	(*input)->std_in = (char **)malloc(sizeof(char *) * 2);
+	str = (*input)->std_in;
+	*str = (char *)malloc(sizeof(char));
+	str[1] = NULL;
+	while (get_next_line(fd, &(*str)))
+	{
+//		(*str)++;
+		(*input)->std_in = ft_realloc((*input)->std_in, ft_twodem_strlen((*input)->std_in));
+
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+}
+
 int main()
 {
+	t_stdin *input;
+	ft_read_write(&input);
 
 
 
