7c36e5da1f7625c7193147b43f42b62e2cfb9ce8
orizhiy <sklif.85@gmail.com>
2017-05-30 18:53:47 +0300

    norm_14

--- data_check.c
+++ data_check.c
@@ -15,7 +15,7 @@ void	need_data_check(t_lemin **lemin)
 		i++;
 	}
 	init_queue_visit(lemin, i);
-	if (!hard_data_check(lemin, -1, NULL, NULL))
+	if (!hard_data_check(lemin, -1, (*lemin)->queue, (*lemin)->visit))
 		ft_error(2);
 }
 
@@ -42,18 +42,22 @@ void	simple_data_check(t_lemin **lemin)
 		ft_error(2);
 }
 
+void hard_data_check_1(char **queue, t_connect **connect, t_room **room)
+{
+	rewrite_queue(&queue);
+	*connect = (*room)->connect;
+	fill_queue(*connect, &queue);
+}
+
 int		hard_data_check(t_lemin **lemin, int j, char **queue, char **visit)
 {
 	t_room		*room;
 	t_connect	*connect;
 
-	queue = (*lemin)->queue;
-	visit = (*lemin)->visit;
 	find_start(lemin, &queue);
 	while (queue[0] && (room = (*lemin)->room))
 	{
 		if (check_visit(queue[0], visit))
-		{
 			while (room->name)
 			{
 				if (!ft_strcmp(queue[0], room->name))
@@ -62,14 +66,11 @@ int		hard_data_check(t_lemin **lemin, int j, char **queue, char **visit)
 					if (check_visit_end(lemin, visit[j]))
 						return (free_visit_and_queue((*lemin)->visit,
 						(*lemin)->queue) == NULL ? 1 : 1);
-					rewrite_queue(&queue);
-					connect = room->connect;
-					fill_queue(connect, &queue);
+					hard_data_check_1(queue, &connect, &room);
 					break ;
 				}
 				room = room->next;
 			}
-		}
 		else
 			rewrite_queue(&queue);
 	}
